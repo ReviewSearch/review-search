37e9965d3e36a0f3ad9b45ad67f83276a308cab5
hj <a301dks@naver.com>
2020-06-08 21:58:21 +0900

    fix: Comment테이블 pull_request ID가 제대로 들어가도록 수정

--- src/main/java/wooteco/review/domain/Comment.java
+++ src/main/java/wooteco/review/domain/Comment.java
@@ -7,40 +7,40 @@ import org.springframework.data.relational.core.mapping.Table;
 
 @Table("COMMENT")
 public class Comment {
-    @Id
-    private final Long id;
-    private final String login;
-    private final String content;
-    private final LocalDateTime updatedAt;
-    private final String htmlUrl;
+	@Id
+	private final Long id;
+	private final String login;
+	private final String content;
+	private final LocalDateTime updatedAt;
+	private final String htmlUrl;
 
-    Comment(final Long id, String login, final String content,
-        final LocalDateTime updatedAt, final String htmlUrl) {
-        this.id = id;
-        this.login = login;
-        this.content = content;
-        this.updatedAt = updatedAt;
-        this.htmlUrl = htmlUrl;
-    }
+	Comment(final Long id, String login, final String content,
+		final LocalDateTime updatedAt, final String htmlUrl) {
+		this.id = id;
+		this.login = login;
+		this.content = content;
+		this.updatedAt = updatedAt;
+		this.htmlUrl = htmlUrl;
+	}
 
-    public static Comment of(final Long id, final String login, final String content,
-        final LocalDateTime updatedAt, final String htmlUrl) {
-        return new Comment(id, login, content, updatedAt, htmlUrl);
-    }
+	public static Comment of(final Long id, final String login, final String content,
+		final LocalDateTime updatedAt, final String htmlUrl) {
+		return new Comment(id, login, content, updatedAt, htmlUrl);
+	}
 
-    public Comment withId(final Long id) {
-        return new Comment(id, this.login, this.content, this.updatedAt, this.htmlUrl);
-    }
+	public Comment withId(final Long id) {
+		return new Comment(id, this.login, this.content, this.updatedAt, this.htmlUrl);
+	}
 
-    public String getLogin() {
-        return login;
-    }
+	public String getLogin() {
+		return login;
+	}
 
-    public String getContent() {
-        return content;
-    }
+	public String getContent() {
+		return content;
+	}
 
-    public String getHtmlUrl() {
-        return htmlUrl;
-    }
+	public String getHtmlUrl() {
+		return htmlUrl;
+	}
 }
--- src/main/java/wooteco/review/domain/PullRequest.java
+++ src/main/java/wooteco/review/domain/PullRequest.java
@@ -5,42 +5,41 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import wooteco.review.service.github.State;
 
 @Table("PULL_REQUEST")
 public class PullRequest {
-    private final Long id;
-    private final Long number;
-    private final LocalDateTime updatedAt;
-    private final State state;
-    @Column("PULL_REQUEST")
-    private final Set<Comment> comments;
+	@Id
+	private final Long id;
+	private final Long number;
+	private final LocalDateTime updatedAt;
+	private final State state;
+	private final Set<Comment> comments;
 
-    PullRequest(Long id, final Long number, final LocalDateTime updatedAt, State state,
-        final Set<Comment> comments) {
-        this.id = id;
-        this.number = number;
-        this.updatedAt = updatedAt;
-        this.state = state;
-        this.comments = comments;
-    }
+	PullRequest(Long id, final Long number, final LocalDateTime updatedAt, State state,
+		final Set<Comment> comments) {
+		this.id = id;
+		this.number = number;
+		this.updatedAt = updatedAt;
+		this.state = state;
+		this.comments = comments;
+	}
 
-    public static PullRequest of(final Long id, final Long number, State state, final LocalDateTime updatedAt) {
-        return new PullRequest(id, number, updatedAt, state, new HashSet<>());
-    }
+	public static PullRequest of(final Long id, final Long number, State state, final LocalDateTime updatedAt) {
+		return new PullRequest(id, number, updatedAt, state, new HashSet<>());
+	}
 
-    public PullRequest withId(final Long id) {
-        return new PullRequest(id, this.number, this.updatedAt, this.state, this.comments);
-    }
+	public PullRequest withId(final Long id) {
+		return new PullRequest(id, this.number, this.updatedAt, this.state, this.comments);
+	}
 
-    public PullRequest withNumber(final Long number) {
-        return new PullRequest(id, number, this.updatedAt, state, this.comments);
-    }
+	public PullRequest withNumber(final Long number) {
+		return new PullRequest(id, number, this.updatedAt, state, this.comments);
+	}
 
-    public PullRequest withComments(final Set<Comment> comments) {
-        return new PullRequest(id, this.number, this.updatedAt, state, comments);
-    }
+	public PullRequest withComments(final Set<Comment> comments) {
+		return new PullRequest(id, this.number, this.updatedAt, state, comments);
+	}
 }
