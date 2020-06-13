0631db5b5dc0afb6422cb1ff2b8511cfb176096d
SeYun <ksy90101@gmail.com>
2020-06-13 13:15:13 +0900

    refactor : cotnent 마크 다운화

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,29 +1,33 @@
 import api from "../api/index.js"
-import { commentTemplate } from "../utils/templates.js";
+import {commentTemplate} from "../utils/templates.js";
 
 const CLICK = 'click';
 
 function Index() {
-    const $submitButton = document.querySelector("#submit-button")
+  const $submitButton = document.querySelector("#submit-button")
 
-    const onSearchKeyword = event => {
-        event.preventDefault()
-        const $comments = document.querySelector("#comments")
-        const keyword = document.querySelector("#keyword").value
+  const onSearchKeyword = event => {
+    event.preventDefault()
+    const $comments = document.querySelector("#comments")
+    const keyword = document.querySelector("#keyword").value
 
-        api.search.getComments(keyword)
-            .then(comments => {
-                $comments.innerHTML = comments.map(comment => commentTemplate(comment)).join('')
-            })
-    }
+    api.search.getComments(keyword)
+      .then(comments => {
+        $comments.innerHTML = comments.map(({login, content, htmlUrl}) => {
+          const markedContent = marked(content);
+          console.log(markedContent);
+          return commentTemplate({login, markedContent, htmlUrl})
+        }).join('')
+      })
+  }
 
-    const initEventListener = () => {
-        $submitButton.addEventListener(CLICK, onSearchKeyword)
-    }
+  const initEventListener = () => {
+    $submitButton.addEventListener(CLICK, onSearchKeyword)
+  }
 
-    this.init = () => {
-        initEventListener();
-    }
+  this.init = () => {
+    initEventListener();
+  }
 }
 
 const index = new Index()
--- src/main/resources/static/utils/templates.js
+++ src/main/resources/static/utils/templates.js
@@ -1,11 +1,11 @@
-export const commentTemplate = comment =>
+export const commentTemplate = ({login, markedContent, htmlUrl}) =>
     `<div class="mx-auto mb-5 mb-lg-0 mb-lg-3 col-12 col-md-6">
         <div class="card">
             <div class="card-body">
-                <h5 class="card-title">${comment.login}</h5>
-                <h6 class="card-subtitle mb-2 text-muted" style="display:none;">${comment.login}</h6>
-                <p id="content" class="card-text">${comment.content}</p>
-                <a href="${comment.htmlUrl}" class="card-link">Github에서 보기</a>
+                <h5 class="card-title">${login}</h5>
+                <h6 class="card-subtitle mb-2 text-muted" style="display:none;">${login}</h6>
+                <p id="content" class="card-text">${markedContent}</p>
+                <a href="${htmlUrl}" class="card-link">Github에서 보기</a>
             </div>
         </div>
     </div>`
