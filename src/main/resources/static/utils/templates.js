1162475dc44756fee37dcc0609f0f4c5978042cb
SeYun <ksy90101@gmail.com>
2020-06-17 15:29:10 +0900

    [#35] refactor : footer 아이콘 변경 및 링크 설정

--- src/main/resources/static/utils/templates.js
+++ src/main/resources/static/utils/templates.js
@@ -1,13 +1,13 @@
 export const commentTemplate = ({login, markedContent, htmlUrl}) =>
-  `<div class="mx-auto mb-5 mb-lg-0 mb-lg-3 col-12 col-md-6">
+  `<div class="mx-auto mb-5 mb-lg-0 mb-lg-3 col-10">
         <div class="card">
-            <div class="card-body">
+            <div class="card-body" >
                 <h5 class="card-title">${login}</h5>
                 <h6 class="card-subtitle mb-2 text-muted" style="display:none;">${login}</h6>
-                <div>
+                <div class="content">
                 <p id="content" class="card-text">${markedContent}</p>
-                <a href="${htmlUrl}" class="card-link">Github에서 보기</a>
                 </div>
+                <a href="${htmlUrl}" class="card-link">Github에서 보기</a>
             </div>
         </div>
     </div>`
