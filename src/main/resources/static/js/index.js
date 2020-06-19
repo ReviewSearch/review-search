089b8cefd1f69796b884a43c2884931b4c075b20
SeYun <ksy90101@naver.com>
2020-06-19 16:47:42 +0900

    [#34,#35,#36] 탭문자로 변경 및 공백 제거

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,39 +1,39 @@
 import api from "../api/index.js"
-import {commentTemplate} from "../utils/templates.js";
+import { commentTemplate } from "../utils/templates.js";
 
 const CLICK = 'click';
 
 function Index() {
-  const $submitButton = document.querySelector("#submit-button")
+    const $submitButton = document.querySelector("#submit-button")
 
-  const onSearchKeyword = event => {
-    event.preventDefault()
-    const $comments = document.querySelector("#comments")
-    const keyword = document.querySelector("#keyword").value
+    const onSearchKeyword = event => {
+        event.preventDefault()
+        const $comments = document.querySelector("#comments")
+        const keyword = document.querySelector("#keyword").value
 
-    api.search.getComments(keyword)
-      .then(comments => {
-        $comments.innerHTML = comments.map(({login, content, htmlUrl}) => {
-          marked.setOptions({
-            highlight: function (code, language) {
-              const validLanguage = hljs.getLanguage(language) ? language : 'plaintext';
-              return hljs.highlight(validLanguage, code).value;
-            }
-          });
+        api.search.getComments(keyword)
+            .then(comments => {
+                $comments.innerHTML = comments.map(({login, content, htmlUrl}) => {
+                    marked.setOptions({
+                        highlight: function (code, language) {
+                            const validLanguage = hljs.getLanguage(language) ? language : 'plaintext';
+                            return hljs.highlight(validLanguage, code).value;
+                        }
+                    });
 
-          const markedContent = marked(content)
-          return commentTemplate({login, markedContent, htmlUrl})
-        }).join('')
-      })
-  }
+                    const markedContent = marked(content)
+                    return commentTemplate({login, markedContent, htmlUrl})
+                }).join('')
+            })
+    }
 
-  const initEventListener = () => {
-    $submitButton.addEventListener(CLICK, onSearchKeyword)
-  }
+    const initEventListener = () => {
+        $submitButton.addEventListener(CLICK, onSearchKeyword)
+    }
 
-  this.init = () => {
-    initEventListener();
-  }
+    this.init = () => {
+        initEventListener();
+    }
 }
 
 const index = new Index()
--- src/main/resources/static/utils/templates.js
+++ src/main/resources/static/utils/templates.js
@@ -1,7 +1,7 @@
 export const commentTemplate = ({login, markedContent, htmlUrl}) =>
-  `<div class="mx-auto mb-5 mb-lg-0 mb-lg-3 col-10">
+    `<div class="mx-auto mb-5 mb-lg-0 mb-lg-3 col-10">
         <div class="card">
-            <div class="card-body" >
+            <div class="card-body">
                 <h5 class="card-title">${login}</h5>
                 <h6 class="card-subtitle mb-2 text-muted" style="display:none;">${login}</h6>
                 <div class="content">
